7b490d6bb6e701856372b01e8bcc004a7b929f4f
Kyle Dinh <kyledinh@gmail.com>
2018-03-07 23:04:58 -0800

    execute journ command

--- docker/run-journ-img.sh
+++ docker/run-journ-img.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-docker run -it --rm --name journ-container journ-img ls /node_modules/journ
+docker run -it --rm \
+  --name journ-container \
+  -v "$PWD":/home/node \
+  -w /home/node \
+  journ-img \
+  /node_modules/journ/journ.js $1
